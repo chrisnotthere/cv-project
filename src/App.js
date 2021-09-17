d4da48c7487d8c044da8a61e5e4bbc3fb395bc89
Chris <mr.chrisnash@gmail.com>
2021-09-17 09:26:24 -0700

    project setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './styles/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <br/><br/>
+      this is my first React App!!
+    </div>
+  );
+}
+
+export default App;

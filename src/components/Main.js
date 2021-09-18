d057e718cf63732aaf29e2002be5dda6bdc37a6f
Chris <mr.chrisnash@gmail.com>
2021-09-18 09:35:09 -0700

    main holds the state and onChange handlers

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,15 +4,45 @@ import CVForm from './CVForm/CVForm';
 import CVPreview from './CVPreview/CVPreview';
 
 class Main extends Component {
+  constructor(props) {
+    super(props)
+
+    // this is the default CV info, before the user enters any information
+    this.state = {
+      name: 'John Wick',
+      description: 'default description'
+    }
+  }
+
+  handleNameChange = (event) => {
+    this.setState({
+      name: event.target.value
+    })
+  }
+
+  handleDescriptionChange = (event) => {
+    this.setState({
+      description: event.target.value
+    })
+  }
 
   render() {
     return (
       <main>
-        <CVForm data='test' />
-        <CVPreview data='test' />
+        <CVForm 
+          name={this.state.name}
+          description={this.state.description}
+          handleNameChange={this.handleNameChange.bind(this)}
+          handleDescriptionChange={this.handleDescriptionChange.bind(this)}
+        />
+        <CVPreview 
+          name={this.state.name} 
+          description={this.state.description}
+        />
       </main>
     )
   }
 }
 
 export default Main;
+

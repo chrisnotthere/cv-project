31e1316482da8e30832ee004541ac9a121868e02
Chris <mr.chrisnash@gmail.com>
2021-09-20 14:22:38 -0700

    creating / formatting cv header

--- src/components/CVPreview/CVPreview.js
+++ src/components/CVPreview/CVPreview.js
@@ -1,49 +1,21 @@
 import '../../styles/CVPreview.css';
 import React, {Component} from "react";
+import Header from './Header';
 
 class CVPreview extends Component {
 
   render() {
     return (
       <div className='cv-preview'>
-        <h2>CVPreview</h2>
-        <div>
+        <Header 
+          name={this.props.name}
+          title={this.props.title}
+          phone={this.props.phone}
+          email={this.props.email}
+          location={this.props.location}
+          description={this.props.description}
+        />
 
-          {this.props.name}
-          <br/>
-          {this.props.title}
-          <br/>
-          {this.props.phone}
-          <br/>
-          {this.props.email}
-          <br/>
-          {this.props.location}
-          <br/>
-          {this.props.description}
-          <br/>
-          {this.props.company}
-          <br/>
-          {this.props.position}
-          <br/>
-          {this.props.workStart}
-          <br/>
-          {this.props.workEnd}
-          <br/>
-          {this.props.workDescription}
-          <br/>
-          {this.props.course}
-          <br/>
-          {this.props.institution}
-          <br/>
-          {this.props.educationStart}
-          <br/>
-          {this.props.educationEnd}
-          <br/>
-          {this.props.educationDescription}
-          <br/>
-
-
-        </div>
       </div>
     )
   }
---
+++ src/components/CVPreview/Header.js
@@ -0,0 +1,26 @@
+import React, {Component} from "react";
+
+class Header extends Component {
+
+  render() {
+    return (
+      <>
+        <header className="cvheader">
+          <div>
+            <p className="name">{this.props.name}</p>
+            <p className="title">{this.props.title}</p>
+          </div>
+          <div className="personal-info">
+            <p>{this.props.phone}</p>
+            <p>{this.props.email}</p>
+            <p>{this.props.location}</p>
+          </div>
+        </header>
+        <hr/>
+        <p>{this.props.description}</p>
+      </>
+    )
+  }
+}
+
+export default Header;
